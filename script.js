c64faea7d108aa72d362d421eccff9f8d8bb927a
Akib-Hasaan <akibhasan582@gmail.com>
2024-11-11 22:57:40 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// Individually selecting
+// Using different symbols, we can separately select the different parts in non-Array data as well
+let x = "Are ki"
+console.log(x.split(""));
+
+let y = "Go-od-bye!"
+console.log(y.split("-"));
+
+let w = "A#ki#b Hasan"
+console.log(w.split("#"));
+
+let v = "Akib Hasan"
+console.log(v.toLocaleUpperCase());
+
+let u = "Akib Hasan"
+console.log(u.toLocaleLowerCase());
